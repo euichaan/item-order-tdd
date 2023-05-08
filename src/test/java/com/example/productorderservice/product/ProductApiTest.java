68a35525588d94e9c3bdbbe6fd7b4897fb103af3
euichanhwang <pine6971@naver.com>
2023-05-09 00:48:30 +0900

    test: RestAssured 를 사용한 API 테스트로 전환한다

---
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -0,0 +1,47 @@
+package com.example.productorderservice.product;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import com.example.productorderservice.ApiTest;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+public class ProductApiTest extends ApiTest {
+
+	@Autowired
+	private ProductService productService;
+
+	@Test
+	void 상품등록() {
+		final var request = 상품등록요청_생성();
+
+		// API 요청
+		final var response = 상품등록요청(request);
+
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+	}
+
+	private ExtractableResponse<Response> 상품등록요청(final AddProductRequest request) {
+		return RestAssured.given().log().all() // 요청을 보내는 로그를 남기겠다
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.body(request)
+			.when()
+			.post("/products")
+			.then()
+			.log().all().extract();
+	}
+
+	private AddProductRequest 상품등록요청_생성() {
+		final String name = "상품명";
+		final int price = 1000;
+		final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+		return new AddProductRequest(name, price, discountPolicy);
+	}
+}
